b23f343adcea33d5317457336852039f269f32a6
magneto <magneto@magneto-studio.lan>
2022-10-06 11:57:31 +0900

    Added tools/setup

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 
 [![clojars badge](https://img.shields.io/clojars/v/ajchemist/github.clj.alpha.svg?style=flat-square)](https://clojars.org/ajchemist/github.clj.alpha)
-[![travis](https://img.shields.io/travis/ajchemist/github.clj.alpha/master.svg?style=flat-square)](https://travis-ci.com/ajchemist/github.clj.alpha)
 
 
 # Tools
--- src/core/github/core/alpha/tools.clj
+++ src/core/github/core/alpha/tools.clj
@@ -8,14 +8,22 @@
    )
   (:import
    java.io.PushbackReader
+   java.io.File
    ))
+
+
+(defn sh-exit!
+  [{:keys [exit out err] :as sh-return}]
+  (println (str err out))
+  (when-not (zero? exit)
+    (throw (ex-info "Non-zero exit." sh-return))))
 
 
 (defn- pass
   "Return non-nil only if the secret exists in `pass-name`"
   [pass-name]
   (let [{:keys [exit out err]} (jsh/sh "pass" "show" pass-name)]
-    (println err)
+    (when err (println err))
     (cond
       (not (zero? exit))
       nil
@@ -26,17 +34,45 @@
       :else (str/trim-newline out))))
 
 
+(def ^File ^:private secrets-pass-edn-file (jio/file ".github" "secrets.pass.edn"))
+
+
 (defn setup-secrets-from-pass
-  [{:keys [basic-auth repository edn-file]
-    :or   {edn-file (jio/file ".github" "secrets.pass.edn")}}]
-  (assert (.exists (jio/as-file edn-file)) (str "edn-file should be exists: " edn-file))
+  [{:keys [basic-auth repository :secrets.pass/edn-file]
+    :or   {edn-file secrets-pass-edn-file}}]
+  (cond
+    (not (.exists (jio/as-file edn-file)))
+    (println "[skip] setup-secrets-from-pass (no such file):" edn-file)
+
+    :else
+    (with-open [rdr (jio/reader (jio/as-file edn-file))]
+      (let [rs (edn/read (PushbackReader. rdr))]
+        (run!
+          (fn [[secret-name pass-name]]
+            (when-some [secret-value (pass pass-name)]
+              (github/actions-put-repo-secret
+                {:basic-auth basic-auth}
+                repository secret-name secret-value)
+              (println pass-name "->" secret-name)))
+          rs)))))
+
+
+(defn setup
+  [{:keys [:repo/edn-file]
+    :or   {edn-file (jio/file ".github" "repo.edn")}
+    :as   opts}]
+  (assert (.exists (jio/as-file edn-file)) (str ":repo/edn-file should be exists: " edn-file))
   (with-open [rdr (jio/reader (jio/as-file edn-file))]
-    (let [rs (edn/read (PushbackReader. rdr))]
-      (run!
-        (fn [[secret-name pass-name]]
-          (when-some [secret-value (pass pass-name)]
-            (github/actions-put-repo-secret
-              {:basic-auth basic-auth}
-              repository secret-name secret-value)
-            (println pass-name " -> " secret-name)))
-        rs))))
+    (let [{:keys [repository basic-auth]} (edn/read (PushbackReader. rdr))
+          _                               (assert (vector? basic-auth))
+          basic-auth                      (update basic-auth 1 pass)
+          name                            (subs repository (inc (str/last-index-of repository "/")))]
+      (github/create-repo {:basic-auth basic-auth} name)
+      (setup-secrets-from-pass
+        (-> opts
+          (dissoc :repo/edn-file)
+          (assoc
+            :basic-auth basic-auth
+            :repository repository)))
+      (sh-exit! (jsh/sh "git" "init"))
+      (sh-exit! (jsh/sh "git" "remote" "add" "-f" "origin" (str "git@github.com:" repository))))))
